60efafe1ace4a6863ffef86066756167b87cadeb
James Brundage <@github.com>
2022-04-20 17:53:35 -0700

    Upating Markdown Help: Adding Help for -WhatIf and -Confirm

--- Formatting/PowerShell.Markdown.Help.format.ps1
+++ Formatting/PowerShell.Markdown.Help.format.ps1
@@ -47,7 +47,7 @@ Write-FormatView -TypeName PowerShell.Markdown.Help -Action {
             '---'
             Format-Markdown -Heading "Parameters:" -HeadingSize 3
 
-            foreach ($parameter in $helpObject.Parameters.parameter) {
+            foreach ($parameter in $helpObject.Parameters.parameter) {                
                 $parameterDisplayName = 
                     if ($parameter.required) {
                         "**$($parameter.Name)**"
@@ -57,6 +57,19 @@ Write-FormatView -TypeName PowerShell.Markdown.Help -Action {
 
                 Format-Markdown -HeadingSize 4 -Heading $parameterDisplayName
 
+                if ($parameterName -in 'WhatIf', 'Confirm') {
+                    "-$($parameterName)" +  
+                        'is an automatic variable that is created when a command has ```[CmdletBinding(SupportsShouldProcess)]```.'
+                    if ($parameterName -eq 'WhatIf') {
+                        "-WhatIf is used to see what would happen, or return operations without executing them"
+                    }
+                    if ($parameterName -eq 'Confirm') {
+                        '-Confirm is used to -Confirm each operation.  If you pass ```-Confirm:$false``` you will not be prompted.'
+                        'If the command sets a ```[ConfirmImpact("Medium")]``` which is lower than ```$confirmImpactPreference```, you will not be prompted unless -Confirm is passed'
+                    }
+                    continue
+                }
+
                 ($parameter.description | Out-String -Width 1mb) -split '(?>\r\n|\n)' -replace '^-\s', '* ' -join [Environment]::NewLine
 
                 [Ordered]@{
@@ -65,7 +78,6 @@ Write-FormatView -TypeName PowerShell.Markdown.Help -Action {
                     Postion = $parameter.position
                     PipelineInput = $parameter.pipelineInput                    
                 } | Format-Markdown
-
             }
         }
     ) -join [Environment]::NewLine
