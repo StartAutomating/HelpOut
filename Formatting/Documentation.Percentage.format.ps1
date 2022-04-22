83d69d298e35f9555c66a8c6b39bc3132bf22bdf
James Brundage <@github.com>
2022-04-21 17:09:36 -0700

    Updating Documentation.Percentage format:  Fixing color

--- Formatting/Documentation.Percentage.format.ps1
+++ Formatting/Documentation.Percentage.format.ps1
@@ -2,6 +2,6 @@ Write-FormatView -TypeName File.Documentation.Percentage, Documentation.Percenta
     CommentPercent = { '' + ([Math]::Round($_.CommentPercent,2)) + '%' }
 } -ColorProperty @{
     CommentPercent = {
-        Format-Heatmap -HeatMapMax 20 -InputObject 20 -HeatMapHot "#11ff11" -HeatMapCool "#ff1111"
+        Format-Heatmap -HeatMapMax 20 -InputObject $_.CommentPercent -HeatMapHot "#11ff11" -HeatMapCool "#ff1111"
     }
 }
