be46deba11672e7268a46e8799b53c5d5c249a6d
James Brundage <@github.com>
2022-04-21 17:06:29 -0700

    Updating Documentation.Percentage format:  Rounding, adding %, and color

--- Formatting/Documentation.Percentage.format.ps1
+++ Formatting/Documentation.Percentage.format.ps1
@@ -1,1 +1,7 @@
-Write-FormatView -TypeName File.Documentation.Percentage, Documentation.Percentage -Property Name,PercentageDocumented -AutoSize
+Write-FormatView -TypeName File.Documentation.Percentage, Documentation.Percentage -Property Name,CommentPercent -AutoSize -VirtualProperty @{
+    CommentPercent = { '' + ([Math]::Round($_.CommentPercent,2)) + '%' }
+} -ColorProperty @{
+    CommentPercent = {
+        Format-Heatmap -HeatMapMax 20 -InputObject 20 -HeatMapHot "#11ff11" -HeatMapCool "#ff1111"
+    }
+}
