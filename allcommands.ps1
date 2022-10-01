653125aa6ce3fd61aed417f33e3d73921135c5a4
StartAutomating <StartAutomating@github.com>
2022-10-01 20:32:45 +0000

    Save-MAML:  Adding [IO.FileInfo] to [OutputType] (re #78)

--- allcommands.ps1
+++ allcommands.ps1
@@ -1291,7 +1291,7 @@ function Save-MAML
         Save-Maml -Module HelpOut -PassThru
     #>
     [CmdletBinding(DefaultParameterSetName='CommandInfo',SupportsShouldProcess=$true)]
-    [OutputType([Nullable])]
+    [OutputType([Nullable], [IO.FileInfo])]
     param( 
     # The name of one or more modules.
     [Parameter(ParameterSetName='ByModule',ValueFromPipelineByPropertyName=$true)]
