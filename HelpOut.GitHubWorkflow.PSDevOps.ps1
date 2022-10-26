c3cff63edbd8ae540b2c74ab6913fabf6dae76dd
James Brundage <@github.com>
2022-10-26 12:15:49 -0700

    Updating workflow definitions (re #82)

--- HelpOut.GitHubWorkflow.PSDevOps.ps1
+++ HelpOut.GitHubWorkflow.PSDevOps.ps1
@@ -1,8 +1,14 @@
 ﻿#requires -Module HelpOut
 #requires -Module PSDevOps
+#requires -Module GitPub
 Push-Location $PSScriptRoot
+
+Import-BuildStep -Module GitPub
 
 New-GitHubWorkflow -Name "Test, Tag, Release, and Publish" -On Demand, Push -Job PowerShellStaticAnalysis, TestPowerShellOnLinux, TagReleaseAndPublish, HelpOut, RunEZOut |
     Set-Content .\.github\workflows\TestReleaseAndPublish.yml -Encoding UTF8 -PassThru
+
+New-GitHubWorkflow -On Issue, Demand -Job RunGitPub -Name OnIssueChanged |
+    Set-Content (Join-Path $PSScriptRoot .github\workflows\OnIssue.yml) -Encoding UTF8 -PassThru
 
 Pop-Location
