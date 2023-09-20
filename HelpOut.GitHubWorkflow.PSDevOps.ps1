57394789bb3483f765701aefb50763475c9158f9
StartAutomating <StartAutomating@github.com>
2023-09-20 03:59:54 +0000

    Updating Module Version [0.4.8] and CHANGELOG
    Fixing PSA credentials (Fixes #144)

--- HelpOut.GitHubWorkflow.PSDevOps.ps1
+++ HelpOut.GitHubWorkflow.PSDevOps.ps1
@@ -10,7 +10,10 @@ Import-BuildStep -Module HelpOut
 New-GitHubWorkflow -Name "Test, Tag, Release, and Publish" -On Demand, Push -Job PowerShellStaticAnalysis, 
     TestPowerShellOnLinux, 
     TagReleaseAndPublish, 
-    BuildHelpOut -OutputPath .\.github\workflows\TestReleaseAndPublish.yml
+    BuildHelpOut -OutputPath .\.github\workflows\TestReleaseAndPublish.yml -Env @{        
+        "AT_PROTOCOL_HANDLE" = "mrpowershell.bsky.social"
+        "AT_PROTOCOL_APP_PASSWORD" = '${{ secrets.AT_PROTOCOL_APP_PASSWORD }}'
+    }
 
 New-GitHubWorkflow -On Demand, 
     Released -Job RunGitPub -Name OnIssueOrRelease -OutputPath .\.github\workflows\GitPub.yml
