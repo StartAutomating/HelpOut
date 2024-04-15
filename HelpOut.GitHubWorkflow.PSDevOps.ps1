d2464fdeb12efb953406e1096fcccb96112dfd26
James Brundage <@github.com>
2024-04-15 10:32:07 -0700

    feat: https://ghcr.io/startautomating/helpout ( Fixes #183 )

--- HelpOut.GitHubWorkflow.PSDevOps.ps1
+++ HelpOut.GitHubWorkflow.PSDevOps.ps1
@@ -10,10 +10,12 @@ Import-BuildStep -Module HelpOut
 New-GitHubWorkflow -Name "Test, Tag, Release, and Publish" -On Demand, Push -Job PowerShellStaticAnalysis, 
     TestPowerShellOnLinux, 
     TagReleaseAndPublish, 
-    BuildHelpOut -OutputPath .\.github\workflows\TestReleaseAndPublish.yml -Env @{        
+    BuildHelpOut -OutputPath .\.github\workflows\TestReleaseAndPublish.yml -Env ([Ordered]@{        
         "AT_PROTOCOL_HANDLE" = "mrpowershell.bsky.social"
-        "AT_PROTOCOL_APP_PASSWORD" = '${{ secrets.AT_PROTOCOL_APP_PASSWORD }}'
-    }
+        "AT_PROTOCOL_APP_PASSWORD" = '${{ secrets.AT_PROTOCOL_APP_PASSWORD }}'    
+        REGISTRY = 'ghcr.io'
+        IMAGE_NAME = '${{ github.repository }}'        
+    })
 
 New-GitHubWorkflow -On Demand, 
     Released -Job RunGitPub -Name OnIssueOrRelease -OutputPath .\.github\workflows\GitPub.yml
