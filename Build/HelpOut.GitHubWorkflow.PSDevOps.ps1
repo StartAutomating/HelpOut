bf113e20c3f8fe9a30672dd1066472db9fcc019b
James Brundage <@github.com>
2024-04-15 10:41:03 -0700

    chore: Moving HelpOut.GitHub, Action, and Workflow into /Build ( Fixes #185 )

--- Build/HelpOut.GitHubWorkflow.PSDevOps.ps1
+++ Build/HelpOut.GitHubWorkflow.PSDevOps.ps1
@@ -1,11 +1,14 @@
-﻿#requires -Module HelpOut
-#requires -Module PSDevOps
+﻿#requires -Module PSDevOps
 #requires -Module GitPub
 Push-Location $PSScriptRoot
 
 Import-BuildStep -Module GitPub
 
-Import-BuildStep -Module HelpOut
+Import-BuildStep -SourcePath (
+    Join-Path $PSScriptRoot 'GitHub'
+) -BuildSystem GitHubWorkflow
+
+Push-Location ($PSScriptRoot | Split-Path)
 
 New-GitHubWorkflow -Name "Test, Tag, Release, and Publish" -On Demand, Push -Job PowerShellStaticAnalysis, 
     TestPowerShellOnLinux, 
