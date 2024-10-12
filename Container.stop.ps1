3f4acbea2142f5bbfd7449a8bc9507928e9c2d1f
James Brundage <+@noreply.github.com>
2024-10-12 13:11:52 -0700

    feat: HelpOut Container.stop.ps1 ( Fixes #194 )

---
+++ Container.stop.ps1
@@ -0,0 +1,9 @@
+<#
+.SYNOPSIS
+    Stops the container.
+.DESCRIPTION
+    This script is called when the container is about to stop.
+
+    It can be used to perform any necessary cleanup before the container is stopped.
+#>
+"Container now exiting, hope this helped!" | Out-Host
